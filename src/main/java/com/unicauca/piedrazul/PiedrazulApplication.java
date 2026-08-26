20e782a09e9ac681edeb246592c53cce3c133b9f
juanp-ux <juanpcollazos@unicauca.edu.co>
2026-08-26 01:19:35 -0500

    Proyecto base

---
+++ src/main/java/com/unicauca/piedrazul/PiedrazulApplication.java
@@ -0,0 +1,13 @@
+package com.unicauca.piedrazul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiedrazulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiedrazulApplication.class, args);
+    }
+
+}
