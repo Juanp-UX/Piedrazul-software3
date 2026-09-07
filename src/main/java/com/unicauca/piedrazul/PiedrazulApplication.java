4c171914f8f083ed37bb907e145759499ab60dff
juanp-ux <juanpcollazos@unicauca.edu.co>
2026-09-06 21:47:52 -0500

    fix anotacion clase main

--- src/main/java/com/unicauca/piedrazul/PiedrazulApplication.java
+++ src/main/java/com/unicauca/piedrazul/PiedrazulApplication.java
@@ -2,9 +2,10 @@ package com.unicauca.piedrazul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.modulith.Modulith;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Modulith
+@EnableScheduling
 public class PiedrazulApplication {
 
     public static void main(String[] args) {
